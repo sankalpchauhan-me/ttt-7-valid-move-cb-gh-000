56df6b0d06f8a9d799510832ea039d2345452da8
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-14 13:15:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 def valid_move?(board, index)
   if(index.between?(0,8))
     return true
-  else position_taken? 
+  elsif(board[index] == "X" || board[index] == "O")
+    return false
   end
 end
 
